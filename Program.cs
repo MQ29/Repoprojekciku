6e93aa73304dc0f7e80b79f14e77581a4ea8413b
MQ29 <matiax2829@gmail.com>
2022-11-05 12:30:49 +0100

    lepiej wytlumaczona petla for

--- Program.cs
+++ Program.cs
@@ -5,37 +5,49 @@ namespace Projekcik
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("kurwa");
+            string[] cars = { "ford", "opel", "audi", "honda" };
 
-            string imie = "";
-
-
-            while (imie == "")
+            for (int i = 0; i < cars.Length; i++)
             {
-
-                Console.Write("Podaj imie");
-                imie = Console.ReadLine();
+                int rank = i + 1;
+                System.Console.WriteLine(rank + "." + cars[i]);
             }
 
 
 
-            Random random = new Random();
-            int num = random.Next(1, 7);
-            System.Console.WriteLine(num);
-
-            string name = "Matt";
-            string username = name.Insert(0, "Mr.");
-            System.Console.WriteLine(username);
-
-            string phonenumber = "123-123-123";
-            phonenumber = phonenumber.Replace("-", " ");
-            System.Console.WriteLine(phonenumber);
 
 
+            // string imie = "";
 
-            car car1 = new car("blue", "ford", 1990);
-            car car2 = new car("red", "ferrari", 2003);
-            car1.drive();
-            car2.drive();
+
+            // while (imie == "")
+            // {
+
+            //     Console.Write("Podaj imie");
+            //     imie = Console.ReadLine();
+            // }
+
+
+
+            // Random random = new Random();
+            // int num = random.Next(1, 7);
+            // System.Console.WriteLine(num);
+
+            // string name = "Matt";
+            // string username = name.Insert(0, "Mr.");
+            // System.Console.WriteLine(username);
+
+            // string phonenumber = "123-123-123";
+            // phonenumber = phonenumber.Replace("-", " ");
+            // System.Console.WriteLine(phonenumber);
+
+
+
+            // car car1 = new car("blue", "ford", 1990);
+            // car car2 = new car("red", "ferrari", 2003);
+            // car1.drive();
+            // car2.drive();
 
 
 
