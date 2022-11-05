c6751cf4aa9d9a7c56a5cda5a113004c5dd00f52
MQ29 <matiax2829@gmail.com>
2022-11-05 11:51:20 +0100

    petle

--- Program.cs
+++ Program.cs
@@ -5,6 +5,33 @@ namespace Projekcik
     {
         static void Main(string[] args)
         {
+
+            string imie = "";
+
+
+            while (imie == "")
+            {
+
+                Console.Write("Podaj imie");
+                imie = Console.ReadLine();
+            }
+
+
+
+            Random random = new Random();
+            int num = random.Next(1, 7);
+            System.Console.WriteLine(num);
+
+            string name = "Matt";
+            string username = name.Insert(0, "Mr.");
+            System.Console.WriteLine(username);
+
+            string phonenumber = "123-123-123";
+            phonenumber = phonenumber.Replace("-", " ");
+            System.Console.WriteLine(phonenumber);
+
+
+
             car car1 = new car("blue", "ford", 1990);
             car car2 = new car("red", "ferrari", 2003);
             car1.drive();
