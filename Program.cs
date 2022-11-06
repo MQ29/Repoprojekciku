b1a2e6294138f1d48a986881adf3b05108552d8c
MQ29 <matiax2829@gmail.com>
2022-11-06 15:30:40 +0100

    number guessing game

--- Program.cs
+++ Program.cs
@@ -5,14 +5,105 @@ namespace Projekcik
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("kurwa");
-            string[] cars = { "ford", "opel", "audi", "honda" }; //tworzymy tabele cars 
+            Random random = new Random();
+            bool playagain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            string response;
 
-            for (int i = 0; i < cars.Length; i++) // for(index czyli "i"; dopoki i jest mniejsze od ilosci elementow w tablicy CARS ;inkrementacja)
+            while (playagain)
             {
-                int rank = i + 1;
-                System.Console.WriteLine(rank + "." + cars[i]);
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    System.Console.WriteLine("guess a number between " + min + " - " + max + ":");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    System.Console.WriteLine("Guess: " + guess);
+
+                    if (guess > number)
+                    {
+                        System.Console.WriteLine(guess + " is to high!");
+                    }
+                    else if (guess < number)
+                    {
+                        System.Console.WriteLine(guess + " is to low!");
+                    }
+                    guesses++;
+
+                }
+                System.Console.WriteLine("Number: " + number);
+                System.Console.WriteLine("You win!");
+                System.Console.WriteLine("Guesses: " + guesses);
+                System.Console.WriteLine("would you like to play again Y/N: ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playagain = true;
+                }
+                else
+                {
+                    playagain = false;
+                }
             }
+            System.Console.WriteLine("thanks for playing!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // string[] cars = new string[4];
+
+            // System.Console.WriteLine("Type in four cars: ");
+
+            // for (int i = 0; i < cars.Length; i++) // for(index czyli "i"; dopoki i jest mniejsze od ilosci elementow w tablicy CARS ;inkrementacja)
+            // {
+            //     cars[i] = Console.ReadLine();
+            // }
+
+            // System.Console.WriteLine("Here they are alphabetically:");
+
+            // Array.Sort(cars);  //sortowanie alfabetycznie
+
+            // for (int i = 0; i < cars.Length; i++)
+            // {
+            //     System.Console.WriteLine(cars[i]);
+            // }
+
+
 
 
 
